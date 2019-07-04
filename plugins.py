0e65eb64934b983bad95df30cee4a78da5f65c54
Alysha-94 <alysha.iannetta@gmail.com>
2019-07-04 14:39:53 +1000

    test posts in pages to have accessible url

--- plugins.py
+++ plugins.py
@@ -4,7 +4,12 @@ from datetime import datetime
 from gilbert.content import Templated, Content
 
 class BlogPost(Templated, Content):
-    title: str
-    author: str
-    posted: typing.Union[None, datetime]
-    template: str = "blog/post.html"
+	title: str
+	author: str
+	posted: typing.Union[None, datetime]
+	modified: typing.Union[None, datetime]
+	category: str
+	tags: list
+	template: str = "blog/post.html"
+	summary: str
+	slug: str
