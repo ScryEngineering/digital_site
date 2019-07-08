995e561f5c27804afacef8a5665b237317362783
Alysha-94 <alysha.iannetta@gmail.com>
2019-07-08 16:53:01 +1000

    change tabs to spaces

--- plugins.py
+++ plugins.py
@@ -4,12 +4,12 @@ from datetime import datetime
 from gilbert.content import Templated, Content
 
 class BlogPost(Templated, Content):
-	title: str
-	authors: list
-	posted: datetime
-	modified: typing.Union[None, datetime]
-	category: str
-	tags: list
-	template: str = "blog/post.html"
-	summary: str
-	draft: bool
+    title: str
+    authors: list
+    posted: datetime
+    modified: typing.Union[None, datetime]
+    category: str
+    tags: list
+    template: str = "blog/post.html"
+    summary: str
+    draft: bool
