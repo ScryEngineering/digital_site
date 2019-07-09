f343dc2d41b81e6c05e3da2281928d077337f580
Alysha-94 <alysha.iannetta@gmail.com>
2019-07-09 13:44:20 +1000

    setup team pages

--- plugins.py
+++ plugins.py
@@ -4,12 +4,20 @@ from datetime import datetime
 from gilbert.content import Templated, Content
 
 class BlogPost(Templated, Content):
-	title: str
-	authors: list
-	posted: datetime
-	modified: typing.Union[None, datetime]
-	category: str
-	tags: list
-	template: str = "blog/post.html"
-	summary: str
-	draft: bool
+    title: str
+    authors: list
+    posted: datetime
+    modified: typing.Union[None, datetime]
+    category: str
+    tags: list
+    template: str = "blog/post.html"
+    summary: str
+    draft: bool
+
+class TeamMember(Templated, Content):
+    name: str
+    role: str
+    location: str
+    template: str="team/team_detail.html"
+    summary: str
+    img: str
